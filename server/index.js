85163ae6e8bbe7cd251870f7eefa70ef3ac971c3
kushendra <kushendraunnam@gmail.com>
2026-05-24 18:42:48 +0530

    updated the port issue

--- server/index.js
+++ server/index.js
@@ -32,6 +32,8 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 // SERVER
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
